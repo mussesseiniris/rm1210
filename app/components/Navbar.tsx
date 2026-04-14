0e75b90140f73f297c217751f68462824ef73b3d
Iris <zhangsi1@myvuw.ac.nz>
2026-04-14 23:29:00 +1200

    init: init my project

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+    <div className="flex justify-end gap-10 my-6 mx-15">
+      <a href="Home">Home</a>
+      <a href="">Portfolio</a>
+    </div>
+  );
+}
