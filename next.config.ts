485e44f6ab0b914067d7e1e9dacd1989737f65ce
Iris <zhangsi1@myvuw.ac.nz>
2026-05-07 20:09:56 +1200

    fix: add CD configuratiob files

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
 
-export default nextConfig;
+const nextConfig = {
+  output: "export",
+  basePath: "/rm1210",        
+  images: { unoptimized: true },
+}
+export default nextConfig
