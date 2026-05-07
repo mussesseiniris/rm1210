899e66535e2d742062da57b90a8302bbe7573411
Iris <zhangsi1@myvuw.ac.nz>
2026-05-08 00:17:34 +1200

    fix image paths

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig = {
   basePath: "/rm1210",
   images: { unoptimized: true },
   env: {
-    NEXT_PUBLIC_BASE_PATH: process.env.NODE_ENV === "production" ? "/rm1210" : "",
+    NEXT_PUBLIC_BASE_PATH: "/rm1210",
   },
 }
 export default nextConfig
